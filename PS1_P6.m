79fbe78b1e1e393f67ef7597e0673f3b7a3576d5
Ukasz47 <150520867+Ukasz47@users.noreply.github.com>
2024-09-30 15:33:33 +0200

    Update PS1_P6.m
    Capital demand solved recursively instead of using capital supply

--- PS1_P6.m
+++ PS1_P6.m
@@ -9,17 +9,17 @@ params.alpha = 0.36;
 params.beta = 0.99;
 params.sigma = 1;
 params.delta = 0.025;
-params.T = 200;
+params.T = 300;
 params.k_bar = ((1-params.beta*(1-params.delta))/(params.alpha*params.beta))^(1/(params.alpha-1));
 params.k_0 = params.k_bar;
 epsilon = 10^(-6);
 h = 10^(-6); %numerical derivative step size
-derv_switch = 1; % 0 - only forward difference used, 1 - average of forward and backward difference
+derv_switch = 0; % 0 - only forward difference used, 1 - average of forward and backward difference
 i = 1;
-r_t = 0.02*ones(params.T+1,1);
+r_t = 0.01*ones(params.T+1,1);
 r_t_new = zeros(params.T+1,1);
 r_t(1) = params.alpha*params.k_0^(params.alpha-1)-params.delta;
-r_t_new(1) = params.alpha*params.k_0^(params.alpha-1)-params.delta;
+    r_t_new(1) = params.alpha*params.k_0^(params.alpha-1)-params.delta;
 J = zeros(params.T,params.T);
 
 %===============================================================================================
@@ -45,6 +45,7 @@ dk = iter(r_t,params);
 while max(abs(dk))/params.k_0 > epsilon
     i = i+1;
     r_t_new(2:params.T+1) = r_t(2:params.T+1) - J\dk;
+    r_t_new = max(r_t_new,10^(-4)*ones(params.T+1,1));
     dk = iter(r_t_new,params);
     s = r_t_new(2:params.T+1) - r_t(2:params.T+1);
     J = J + dk*s'/(s'*s);
@@ -66,11 +67,17 @@ function dk = iter(r_t,params)
     k_t = ((r_t+params.delta)/params.alpha).^(1/(params.alpha-1));
     c_bar = params.k_bar^params.alpha-params.delta*params.k_bar;
     c_t(params.T+1) = c_bar;
-    k_s_t = zeros(params.T,1);
+    k_s_t = zeros(params.T+1,1);
+    k_s_t(params.T+1) = k_t(params.T+1);
+
+    options = optimoptions('lsqnonlin', 'Display', 'off');
+    lb = 0;  
+    ub = [];
+
     for t = params.T:-1:1
         c_t(t) = c_t(t+1)/(params.beta*(params.alpha*k_t(t+1)^(params.alpha-1)+1-params.delta));
-        bc = @(k) feasibility(k,k_t(t+1),c_t(t),params); %auxiiary function to transmit consumption and future capital as parameters
-        k_s_t(t) = fsolve(bc,params.k_bar);
+        bc = @(k) feasibility(k,k_s_t(t+1),c_t(t),params); %auxiiary function to transmit consumption and future capital as parameters
+        k_s_t(t) = lsqnonlin(bc, k_s_t(t+1), lb, ub, options);
     end
-    dk = k_s_t-k_t(1:params.T);
+    dk = k_s_t(1:params.T)-k_t(1:params.T);
 end
